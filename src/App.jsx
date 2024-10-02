c74e30b8f09b8c865d2b4f0be0e3cf60ceab0313
SuzyKenne <suzyndatewo@gmail.com>
2024-10-02 14:58:59 +0100

    App content refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import ProfileSideBar from "./components/sideBar/ProfileSideBar";
+import TopBar from "./components/sideBar/TopBar";
+import './App.css';
+import FollowSuggestions from "./components/sideBar/FollowSuggestions";
+import Header from "./components/main-component/Header";
+
+
+
 function App() {
   return (
-    <>
-      <h1 className="text-center text-3xl font-bold underline">Hello World</h1>
-    </>
+    <div className="font-helvetica bg-[#131415] min-h-screen text-[#d9e4ed]">
+      <TopBar />
+      <div className="flex">
+        <ProfileSideBar />
+        <main className="flex-1 p-4">
+          <Header />
+        </main>
+        <FollowSuggestions />
+      </div>
+    </div>
   );
 }
 
