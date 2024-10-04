f3dabd9e8057c063ea50f880a32395e2677b0ef3
SuzyKenne <suzyndatewo@gmail.com>
2024-10-04 04:12:20 +0100

    removed all the svg link and replace with icon component

--- src/components/sideBar/NavLinkList.jsx
+++ src/components/sideBar/NavLinkList.jsx
@@ -1,67 +1,77 @@
-import SuggestedPageList from "./SuggestedPageList"
-
+import {
+  HomeIcon,
+  UsersIcon,
+  MapIcon,
+  VideoCameraIcon,
+  PhotoIcon,
+  MapPinIcon,
+} from '@heroicons/react/16/solid';
+import SuggestedPageList from './SuggestedPageList';
 
 function NavLinkList() {
   return (
     <div className="bg-[#232323] p-3 rounded-md">
       <nav className="mb-6">
         <ul>
-           <li className="mb-2">
-              <a href="#" className="flex items-center text-gray-400 hover:text-[#ffd37d]">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                </svg>
-                Feed
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="#" className="flex items-center text-gray-400 hover:text-[#ffd37d]">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
-                </svg>
-                Friends
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="#" className="flex items-center text-[#ffd37d]">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-                </svg>
-                Hikes
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="#" className="flex items-center text-gray-400 hover:text-[#ffd37d]">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
-                </svg>
-                Videos
-              </a>
-            </li>
-            <li className="mb-2  flex justify-between">
-              <a href="#" className="flex items-center text-gray-400  hover:text-[#ffd37d]">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
-                </svg>
-                Photos
-              </a> 
-              <span className="bg-red-500 text-white w-5 h-5 text-[12px]  rounded-full">+2</span>             
-            </li>
-            <li>
-              <a href="#" className="flex items-center text-gray-400 hover:text-[#ffd37d]">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clipRule="evenodd" />
-                </svg>
-                Map
-              </a>
-            </li>
+          <li className="mb-2">
+            <a
+              href="#"
+              className="flex items-center text-gray-400 hover:text-[#ffd37d]"
+            >
+              <HomeIcon className="h-5 w-5 mr-2" />
+              Feed
+            </a>
+          </li>
+          <li className="mb-2">
+            <a
+              href="#"
+              className="flex items-center text-gray-400 hover:text-[#ffd37d]"
+            >
+              <UsersIcon className="h-5 w-5 mr-2" />
+              Friends
+            </a>
+          </li>
+          <li className="mb-2">
+            <a href="#" className="flex items-center text-[#ffd37d]">
+              <MapPinIcon className="h-5 w-5 mr-2" />
+              Hikes
+            </a>
+          </li>
+          <li className="mb-2">
+            <a
+              href="#"
+              className="flex items-center text-gray-400 hover:text-[#ffd37d]"
+            >
+              <VideoCameraIcon className="h-5 w-5 mr-2" />
+              Videos
+            </a>
+          </li>
+          <li className="mb-2  flex justify-between">
+            <a
+              href="#"
+              className="flex items-center text-gray-400  hover:text-[#ffd37d]"
+            >
+              <PhotoIcon className="h-5 w-5 mr-2" />
+              Photos
+            </a>
+            <span className="bg-red-500 text-white w-5 h-5 text-[12px]  rounded-full">
+              +2
+            </span>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center text-gray-400 hover:text-[#ffd37d]"
+            >
+              <MapIcon className="h-5 w-5 mr-2" />
+              Map
+            </a>
+          </li>
         </ul>
       </nav>
       <SuggestedPageList />
     </div>
-    
-    
-  )
+  );
 }
 
-export default NavLinkList
+export default NavLinkList;
--- src/components/sideBar/TopBar.jsx
+++ src/components/sideBar/TopBar.jsx
@@ -1,27 +1,15 @@
-
+import {
+  PencilSquareIcon,
+  BellIcon,
+  ChatBubbleBottomCenterTextIcon,
+  Cog6ToothIcon,
+} from '@heroicons/react/16/solid';
 const TopBar = () => {
   return (
     <div className="flex items-center justify-between p-3 bg-[#232323] text-[#d9e4ed]">
       <div className="flex items-center">
         <div className="bg-[#ffd37d] rounded-lg p-2 mr-4">
-          <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              viewBox="0 0 512 512"
-              className="h-6 w-6 cursor-pointer hover:text-[#ffd37d]" fill="none"  stroke="currentColor"
-          >
-            <path 
-            d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 
-            33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 
-            33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 
-            6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 
-            8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 
-            23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 
-            28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 
-            103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 
-            88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0
-            112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 
-            17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/>
-          </svg>
+          <PencilSquareIcon className="h-5 w-5 mr-2 text-[#232323]" />
         </div>
         <input
           type="text"
@@ -36,19 +24,19 @@ const TopBar = () => {
         <button className="hover:text-[#ffd37d]">Account</button>
       </div>
       <div className="flex items-center space-x-4">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer hover:text-[#ffd37d]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer hover:text-[#ffd37d]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
+        <ChatBubbleBottomCenterTextIcon className="h-5 w-5 mr-2" />
+        <BellIcon className="h-5 w-5 mr-2" />
+        <Cog6ToothIcon className="h-5 w-5 mr-2" />
         <div className="w-8 h-8 rounded-full  flex items-center justify-center">
-         <img src="/imagees/girl.jpg" alt="User avatar" className="w-14 h-14 rounded-sm m-2" />
+          <img
+            src="/imagees/girl.jpg"
+            alt="User avatar"
+            className="w-14 h-14 rounded-sm m-2"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TopBar;
