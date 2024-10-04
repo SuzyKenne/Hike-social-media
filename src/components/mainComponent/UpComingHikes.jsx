ace42c347d575c3b64fc07c8612154d5c1bb2dbd
SuzyKenne <suzyndatewo@gmail.com>
2024-10-04 10:38:16 +0100

    remove the displaHikes variable

--- src/components/mainComponent/UpComingHikes.jsx
+++ src/components/mainComponent/UpComingHikes.jsx
@@ -1,36 +1,24 @@
-import HikeCard from "./HikeCard";
-import data from "../../../data";
+import HikeCard from './HikeCard';
+import { hikes } from '../../../data';
 
 const UpComingHikes = () => {
-
-  
-
-  
-
-  const displayHikeInfo = data.map((hike) => {
-    return ( 
-      <HikeCard 
-          mountain={hike.mountain}
-          date={hike.date}
-          location={hike.location}
-          time={hike.time}
-          hikeDetails={hike.hikeDetails}
-          image={hike.image}
-          key={hike.id}    
-      />
-    )
-  })
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2  gap-6 ">
-      {displayHikeInfo}
+      {hikes.map((hike) => {
+        return (
+          <HikeCard
+            mountain={hike.mountain}
+            date={hike.date}
+            location={hike.location}
+            time={hike.time}
+            hikeDetails={hike.hikeDetails}
+            image={hike.image}
+            key={hike.id}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default UpComingHikes;
-
-
-
-
- 
